ee5306ddc3b1dd68577e06ac24ca8bcf2df76758
DWalterJansen <40320356+DWalterJansen@users.noreply.github.com>
2018-10-02 08:05:46 -0300

    Trabalho 2 Tópicos IC

--- rastrigin.py
+++ rastrigin.py
@@ -10,8 +10,6 @@ import numpy as np
 def rastrigin(X):
     return 10 * len(X) + sum([(x**2 - 10 * np.cos(2 * math.pi * x)) for x in X])
 
-X = np.linspace(-5.12, 5.12, 4)
-
 limites = [(-5.12, 5.12), (-5.12, 5.12), (-5.12, 5.12), (-5.12, 5.12)]
 
 result = differential_evolution(rastrigin, limites)
